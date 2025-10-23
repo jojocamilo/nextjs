c35d5c5367266165a31bb551fbce13f0cd74a035
JoaquinCamilo <163370967+JoaquinCamilo@users.noreply.github.com>
2025-10-23 11:07:57 +0700

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/logo.png" />
       </head>
       <body>
         <DarkModeProvider>
